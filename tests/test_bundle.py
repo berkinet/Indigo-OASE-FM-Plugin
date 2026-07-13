a63e55c9dc873f6149817d96374a8a98033b6752
berkinet <berkinet@users.noreply.github.com>
2026-07-13 15:01:13 +0200

    Update plugin for RPM sensor fix

--- tests/test_bundle.py
+++ tests/test_bundle.py
@@ -25,7 +25,7 @@ class BundleTests(unittest.TestCase):
             info = plistlib.load(stream)
 
         self.assertEqual(info["ServerApiVersion"], "3.8")
-        self.assertEqual(info["PluginVersion"], "0.2.1")
+        self.assertEqual(info["PluginVersion"], "0.2.2")
         self.assertEqual(
             info["CFBundleIdentifier"],
             "com.berkinet.indigoplugin.oase-fm",
